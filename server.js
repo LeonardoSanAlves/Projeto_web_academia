26f0dcfb768256d603ca2f5293e2db0879d737db
Leonardo Sanches Alves <leonarsanalves@gmail.com>
2025-07-03 16:33:45 -0300

    Backend - db

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 import express from 'express' 
+import { PrismaClient } from './generated/prisma/index.js';
+
+const prisma = new PrismaClient();
 
 const app = express()
 app.use(express.json())
 
 
-const users = []
 
-app.post('/contracts', (req,res)=> {
 
-users.push(req.body)
+app.post('/contracts', async (req,res)=> {
+
+    await prisma.contracts.create({
+        data:{
+               email: req.body.email, 
+               name: req.body.name, 
+               age: req.body.age
+
+        }
+
+
+
+    })
 
 res.status(201).json(req.body)
 
@@ -16,11 +29,68 @@ res.status(201).json(req.body)
 
 )
 
-app.get('/contracts',(req,res)=> {
+app.get('/contracts',async (req,res)=> {
+    let users = []
+
+    if(req.query){
+        users = await prisma.contracts.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age
+            }
+        })
+
+    }
+    
+    else {
+              users = await prisma.contracts.findMany()
+
+        }
+    
+
     res.status(200).json(users)
 
 })
 
+
+
+app.put('/contracts/:id', async (req,res)=> {
+
+    await prisma.contracts.update({
+        where: {
+            id: req.params.id
+        },
+
+
+        
+        data:{
+               email: req.body.email, 
+               name: req.body.name, 
+               age: req.body.age
+
+        }
+
+
+
+    })
+
+res.status(201).json(req.body)
+
+})
+
+
+app.delete('/contracts/:id', async (req, res) => {
+
+    await prisma.contracts.delete({
+        where: {
+            id: req.params.id
+
+        }
+    })
+
+    req.status(200).json( {message: 'Contract deletado com sucesso'})
+})
 
 
 app.listen(3000)
